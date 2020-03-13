59cad6c270ad876d8c950361fc5d23432b117a63
Nishant Singh <59526404+Nishantsingh9412@users.noreply.github.com>
2020-03-14 00:51:22 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!-- index.html -->
+<!DOCTYPE html>
+<html>
+<head>
+	<title>  Input form UI design   </title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+	<script src="https://kit.fontawesome.com/a8c5a6da32.js" crossorigin="anonymous">
+</script>
+
+</head>
+<body>
+	
+	<div class="box">
+		<h2> Login </h2>
+		<form> 
+			<div class="inputBox">
+				<input type="text" name="" required="">
+				<label> Username </label>
+			</div>
+
+			<div class="inputBox">
+				<input type="password" name="" required="">
+				<label> password </label>
+			</div>
+			<input class=" bl" type="submit" name="submit" value="Submit">
+		</form>
+
+	</div>
+
+
+</body>
+</html>
